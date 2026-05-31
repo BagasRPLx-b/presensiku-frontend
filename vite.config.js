441aab92ceea68a99bbd1857e591daa9d299ac03
DESKTOP-DTBGD7S\USER <dimassatriap45@gmail.com>
2026-05-31 19:58:01 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Menaikkan batas toleransi ukuran file dari 500kB menjadi 1600kB (1.6MB)
+    chunkSizeWarningLimit: 1600,
+  },
 })
